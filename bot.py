21c130add32fe8ff53bab8f0b8281fb57418ae53
mosharofroydev <mosharofroydev@gmail.com>
2025-08-28 15:54:53 +0600

    Update and rename bot. to bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
-from pyrogram import Client, filters
+from pyrogra. from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # ==== আপনার তথ্য ====
-API_ID = 24776633   # এখানে আপনার API_ID দিন
-API_HASH = "57b1f632044b4e718f5dce004a988d69"  # এখানে আপনার API_HASH দিন
-BOT_TOKEN = "8412041699:AAGr1YauyYo7g9iHt6vNtKATt3s_fklqaCc"  # নতুন Bot Token
-CHANNEL_ID = -1002912984408   # আপনার প্রাইভেট চ্যানেল ID
+API_ID = 24776633   # আপনার API_ID
+API_HASH = "57b1f632044b4e718f5dce004a988d69"  # আপনার API_HASH
+BOT_TOKEN = "8412041699:AAGRNcKvdlJpLuwmxdDBqabdKq8BDVLt3hU"  # BotFather থেকে কপি করা টোকেন
+CHANNEL_ID = -1002912984408   # আপনার Log Channel ID
 
 # ==== বট তৈরি ====
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
