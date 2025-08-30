74b1f400bb9c9a20b33ba5f783c914fa0f09d554
mosharofroydev <mosharofroydev@gmail.com>
2025-08-30 14:07:00 +0000

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,28 @@
-API_ID = 24776633
-API_HASH = "57b1f632044b4e718f5dce004a988d69"
-BOT_TOKEN = "8210471056:AAEc76RNEX1w32M7WfyY3R8uKzEBy4aOb8s"
-CHANNEL_ID = 1002912984408  # Telegram চ্যানেলের ID
-SEARCH_TIMEOUT = 120  # সার্চ রেজাল্ট auto-delete সময় (সেকেন্ডে)
+# (c) @RoyalKrrishna
+
+class Config(object):
+    # Telegram API
+    API_ID = 24776633
+    API_HASH = "57b1f632044b4e718f5dce004a988d69"
+
+    # BotFather Token
+    BOT_TOKEN = "8210471056:AAEc76RNEX1w32M7WfyY3R8uKzEBy4aOb8s"
+
+    # Bot Username (without @)
+    BOT_USERNAME = "jacpotfilmbot_bot"
+
+    # Owner Telegram ID
+    BOT_OWNER = 7170990925
+
+    # Channel where videos are stored
+    CHANNEL_ID = -1002912984408  # content channel ID
+
+    # Updates Channel for ForceSub
+    UPDATES_CHANNEL = -1002995070932
+    UPDATES_CHANNEL_USERNAME = "uddineeowner"
+
+    # Auto delete time (seconds)
+    AUTO_DELETE_TIME = 60
+
+    # Force Sub On/Off
+    FORCE_SUB = "True"
